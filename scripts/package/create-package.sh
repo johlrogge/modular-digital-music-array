8e11d25c7e2a27014167e80fc9df76dd4bd34837
Joakim Ohlrogge <joakim.ohlrogge@agical.se>
2026-03-20 11:58:16 +0100

    fix(packaging): update beacon Cargo.toml path from bases/ to projects/

--- scripts/package/create-package.sh
+++ scripts/package/create-package.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 exec "$SCRIPT_DIR/create-service-package.sh" \
     --bin beacon \
     --desc "MDMA provisioning beacon" \
-    --cargo-toml bases/beacon/Cargo.toml \
+    --cargo-toml projects/mdma-beacon/Cargo.toml \
     --deps "avahi>=0 dbus>=0" \
     --extra-install "$SCRIPT_DIR/install/beacon.sh"
